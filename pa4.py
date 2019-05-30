e401385c017d3a2cc22278417e3629a8a9ccdce3
Joshua Kang <guppykang@gmail.com>
2019-05-30 00:27:53 -0700

    took out print statements

--- pa4.py
+++ pa4.py
@@ -73,8 +73,8 @@ def kernenlizedPerceptron(trainingSet, trainingLabels, p, isUnique):
     #print(trainingLabels)
 
     for i in range(len(trainingSet)):
-        print('')
-        print('ROUND : ' + str(i) + ' OF ' + str(len(trainingSet)))
+        # print('')
+        # print('ROUND : ' + str(i) + ' OF ' + str(len(trainingSet)))
         # for item in w: 
         #     print(trainingSet.index(item[0]))
 
@@ -91,8 +91,8 @@ def kernenlizedPerceptron(trainingSet, trainingLabels, p, isUnique):
 def getAccuracy(w, testingSet, testingLabels, p, isUnique):
     numCorrect = 0
     for i in range(len(testingSet)):
-        print('')
-        print('Testing ROUND : ' + str(i) + ' OF ' + str(len(testingSet)))
+        # print('')
+        # print('Testing ROUND : ' + str(i) + ' OF ' + str(len(testingSet)))
 
         prediction = predict(testingSet[i], w, p, isUnique) 
         if int(prediction) == testingLabels[i]:
@@ -109,43 +109,43 @@ def getAccuracy(w, testingSet, testingLabels, p, isUnique):
 trainFeatures = []
 trainLabels = []
 
-#loadData('pa4train.txt', trainFeatures, trainLabels)
-loadData('testing.txt', trainFeatures, trainLabels)
+loadData('pa4train.txt', trainFeatures, trainLabels)
+#loadData('testing.txt', trainFeatures, trainLabels)
 
 
-print("p = 2: ")
-classifier = kernenlizedPerceptron(trainFeatures, trainLabels, 2, True)
-accuracy = getAccuracy(classifier, trainFeatures, trainLabels, 2, True)
+# print("p = 2: ")
+# classifier = kernenlizedPerceptron(trainFeatures, trainLabels, 2, True)
+# accuracy = getAccuracy(classifier, trainFeatures, trainLabels, 2, True)
+# print(accuracy)
+
+print("p = 3: ")
+classifier = kernenlizedPerceptron(trainFeatures, trainLabels, 3, True)
+accuracy = getAccuracy(classifier, trainFeatures, trainLabels, 3, True)
 print(accuracy)
 
-# print("p = 3: ")
-# classifier = kernenlizedPerceptron(trainFeatures, trainLabels, 3, True)
-# accuracy = getAccuracy(classifier, trainFeatures, trainLabels, 3, True)
-# print(accuracy)
+print("p = 4: ")
+classifier = kernenlizedPerceptron(trainFeatures, trainLabels, 4, True)
+accuracy = getAccuracy(classifier, trainFeatures, trainLabels, 4, True)
+print(accuracy)
 
-# print("p = 4: ")
-# classifier = kernenlizedPerceptron(trainFeatures, trainLabels, 4, True)
-# accuracy = getAccuracy(classifier, trainFeatures, trainLabels, 4, True)
-# print(accuracy)
+print("p = 5: ")
+classifier = kernenlizedPerceptron(trainFeatures, trainLabels, 5, True)
+accuracy = getAccuracy(classifier, trainFeatures, trainLabels, 5, True)
+print(accuracy)
 
-# print("p = 5: ")
-# classifier = kernenlizedPerceptron(trainFeatures, trainLabels, 5, True)
-# accuracy = getAccuracy(classifier, trainFeatures, trainLabels, 5, True)
-# print(accuracy)
+print("p = 3: ")
+classifier = kernenlizedPerceptron(trainFeatures, trainLabels, 3, False)
+accuracy = getAccuracy(classifier, trainFeatures, trainLabels, 3, False)
+print(accuracy)
 
-# print("p = 3: ")
-# classifier = kernenlizedPerceptron(trainFeatures, trainLabels, 3, False)
-# accuracy = getAccuracy(classifier, trainFeatures, trainLabels, 3, False)
-# print(accuracy)
+print("p = 4: ")
+classifier = kernenlizedPerceptron(trainFeatures, trainLabels, 4, False)
+accuracy = getAccuracy(classifier, trainFeatures, trainLabels, 4, False)
+print(accuracy)
 
-# print("p = 4: ")
-# classifier = kernenlizedPerceptron(trainFeatures, trainLabels, 4, False)
-# accuracy = getAccuracy(classifier, trainFeatures, trainLabels, 4, False)
-# print(accuracy)
-
-# print("p = 5: ")
-# classifier = kernenlizedPerceptron(trainFeatures, trainLabels, 5, False)
-# accuracy = getAccuracy(classifier, trainFeatures, trainLabels, 5, False)
-# print(accuracy)
+print("p = 5: ")
+classifier = kernenlizedPerceptron(trainFeatures, trainLabels, 5, False)
+accuracy = getAccuracy(classifier, trainFeatures, trainLabels, 5, False)
+print(accuracy)
 
 
